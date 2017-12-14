936d48500a1efa87ccac73994e12d2df0370889a
Andrei <andreibalzan@gmail.com>
2017-12-14 18:54:08 +0100

    configuration circleci for project

---
+++ src/main/java/br/com/andrei/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package br.com.andrei.repository;
+
+import br.com.andrei.domain.PersistentToken;
+import br.com.andrei.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
