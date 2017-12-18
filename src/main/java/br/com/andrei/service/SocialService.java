63643576bd449db7ad2a799318f36c74490128b0
Andrei <andreibalzan@gmail.com>
2017-12-18 20:06:06 +0100

    facebook login configuration

--- src/main/java/br/com/andrei/service/SocialService.java
+++ src/main/java/br/com/andrei/service/SocialService.java
@@ -4,7 +4,6 @@ import br.com.andrei.domain.Authority;
 import br.com.andrei.domain.User;
 import br.com.andrei.repository.AuthorityRepository;
 import br.com.andrei.repository.UserRepository;
-import br.com.andrei.security.AuthoritiesConstants;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -17,6 +16,9 @@ import org.springframework.social.connect.UserProfile;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Optional;
@@ -36,6 +38,27 @@ public class SocialService {
     private final UserRepository userRepository;
 
     private final MailService mailService;
+
+    @PostConstruct
+    private void init() {
+        try {
+            String[] fieldsToMap = {
+                "id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email", "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type","is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format","political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other","sports", "test_group", "timezone", "third_party_id", "updated_time", "verified", "viewer_can_send_gift","website", "work"
+            };
+
+            Field field = Class.forName("org.springframework.social.facebook.api.UserOperations").
+                getDeclaredField("PROFILE_FIELDS");
+            field.setAccessible(true);
+
+            Field modifiers = field.getClass().getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            field.set(null, fieldsToMap);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public SocialService(UsersConnectionRepository usersConnectionRepository, AuthorityRepository authorityRepository,
             PasswordEncoder passwordEncoder, UserRepository userRepository,
@@ -95,7 +118,7 @@ public class SocialService {
         String login = getLoginDependingOnProviderId(userProfile, providerId);
         String encryptedPassword = passwordEncoder.encode(RandomStringUtils.random(10));
         Set<Authority> authorities = new HashSet<>(1);
-        authorities.add(authorityRepository.findOne(AuthoritiesConstants.USER));
+        authorities.add(authorityRepository.findOne("ROLE_USER"));
 
         User newUser = new User();
         newUser.setLogin(login);
